20d19ea790f969c9eb00b954c85cde74999454c8
Ariano2700 <kawuai123loco123@gmail.com>
2023-11-06 08:28:44 -0500

    Controlador alimento actualizado

--- src/main/java/com/oma2/oma20/controladores/AlimentoControlador.java
+++ src/main/java/com/oma2/oma20/controladores/AlimentoControlador.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/oma")
+@RequestMapping("/api/alimento")
 public class AlimentoControlador {
 
     @Autowired
